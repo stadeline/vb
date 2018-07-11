2471ee532572f76b7c4ef2fa5c91bd54080f7c5b
0reo27 <stopspammingme@hotmail.ph>
2018-07-11 21:17:01 +0800

    Initial Commit

---
+++ ArithmeticOperations.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Function Evaluate(ByVal firstNumber As String, ByVal secondNumber As String, ByVal operation As String)
+
+        'Define variables
+        Dim returnedResult, firstNumberLocal, secondNumberLocal As Double
+
+        'Check if inputs are numbers
+        If IsNumeric(firstNumber) And IsNumeric(secondNumber) Then
+
+            'Define Local Variables
+            firstNumberLocal = Val(firstNumber)
+            secondNumberLocal = Val(secondNumber)
+
+            'Check which operation is being performed.
+            Select Case operation
+                Case "add"
+                    returnedResult = firstNumberLocal + secondNumberLocal
+                Case "subtract"
+                    returnedResult = firstNumberLocal - secondNumberLocal
+                Case "multiply"
+                    returnedResult = firstNumberLocal * secondNumberLocal
+                Case "divide"
+                    returnedResult = firstNumberLocal / secondNumberLocal
+                Case "modulus"
+                    returnedResult = firstNumberLocal Mod secondNumberLocal
+            End Select
+
+        Else
+
+            MsgBox("Please input a valid number")
+
+        End If
+
+        'Return the result.
+        'Modulus doesn't have decimal places.
+        If operation Is "modulus" Then
+            Return returnedResult
+        Else
+            'Any other operation may have 2.
+            Return Format(returnedResult, "#,##0.00")
+        End If
+
+
+
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        outResult.Text = Evaluate(inFirstNumber.Text, inSecondNumber.Text, "add")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        outResult.Text = Evaluate(inFirstNumber.Text, inSecondNumber.Text, "subtract")
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        outResult.Text = Evaluate(inFirstNumber.Text, inSecondNumber.Text, "multiply")
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        outResult.Text = Evaluate(inFirstNumber.Text, inSecondNumber.Text, "divide")
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        outResult.Text = Evaluate(inFirstNumber.Text, inSecondNumber.Text, "modulus")
+    End Sub
+End Class
---
+++ PersonalInfo.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub btnSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        'Text boxes
+        outName.Text = inputName.Text
+        outAge.Text = inputAge.Text
+        outDOB.Text = FormatDateTime(dtpDob.Value, DateFormat.ShortDate)
+        'Radio Button
+        If rdbMale.Checked Then outGender.Text = rdbMale.Text
+        If rdbFemale.Checked Then outGender.Text = rdbFemale.Text
+        'Combo Box
+        outCourse.Text = cbxCourse.Text
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        inputAge.Text = ""
+        inputName.Text = ""
+        cbxCourse.Text = ""
+        rdbMale.Checked = False
+        rdbFemale.Checked = False
+        dtpDob.Value = "01/01/2000"
+        outName.Text = ""
+        outAge.Text = ""
+        outCourse.Text = ""
+        outDOB.Text = ""
+        outGender.Text = ""
+        outDOB.Text = ""
+    End Sub
+
+    Private Sub btnQuit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuit.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
